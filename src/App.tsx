0c66fee25e4b3de73b30e1e1d347098b99bbf659
Cristina Brunella Céspedes Malca <91429548+CrisCespedes21@users.noreply.github.com>
2025-01-09 00:00:57 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/home/components/Homepage"
+
+export default function App() {
+  return (
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ src/pages/home/components/Homepage.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/layouts/layout";
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <h1>Home Page</h1>
+    </Layout>
+  );
+}
