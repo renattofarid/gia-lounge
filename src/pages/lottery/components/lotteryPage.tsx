526816daa1fc7f52ca8dd0a9ee2feacc36e0d888
Cristina Brunella Céspedes Malca <ccespedesma@unprg.edu.pe>
2025-06-24 11:59:45 -0500

    Update lotteryPage.tsx

--- src/pages/lottery/components/lotteryPage.tsx
+++ src/pages/lottery/components/lotteryPage.tsx
@@ -15,7 +15,6 @@ import CreateLotteryForm from "./addLottery"
 import { useLotteryStore } from "../lib/lottery.store"
 import type { Prize, LotteryItem } from "../lib/lottery.interface"
 import ModalPremios from "./modalPrizes"
-import ModalParticipantes from "./modalParticipants"
 import { useComapanyStore } from "@/pages/company/lib/company.store"
 import { Card } from "@/components/ui/card"
 import DeleteDialog from "@/components/delete-dialog"
@@ -23,6 +22,7 @@ import { errorToast, successToast } from "@/lib/core.function"
 import { deleteLottery } from "../lib/lottery.actions"
 import { Pagination } from "@/components/pagination"
 import ModalWinners from "./modalWinners"
+import ModalParticipantes from "./ModalParticipants"
 
 export default function LotteryPage() {
   const navigate = useNavigate()
